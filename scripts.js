75d535f2364033ce6f0ac6611bfbceacbd13c0a9
Geovana Souza <geovanasouza0712@gmail.com>
2024-07-11 09:10:26 -0300

    novas alterações

--- scripts.js
+++ scripts.js
@@ -1,24 +1,29 @@
-document.getElementById('openButton').addEventListener('click', function() {
-    document.getElementById('contentContainer').style.display = 'flex';
-    this.style.display = 'none';
+document.addEventListener('DOMContentLoaded', function() {
+    const openButton = document.getElementById('openButton');
+    const contentContainer = document.getElementById('contentContainer');
 
-// Inicia os confetes com mais partículas e duração maior
-confetti({
-    particleCount: 600,
-    spread: 250,
-    origin: { y: 0.6 },
-    decay: 0.9, // diminui a velocidade de desaparecimento dos confetes
-    scalar: 1.6, // aumenta o tamanho dos confetes
-});
+    openButton.addEventListener('click', function() {
+        contentContainer.style.display = 'flex'; // Mostra o conteúdo ao clicar no botão
+        openButton.style.display = 'none'; // Esconde o botão após mostrar o conteúdo
 
-// Adiciona um segundo disparo de confetes para um efeito prolongado
-setTimeout(function() {
-    confetti({
-        particleCount: 600,
-        spread: 250,
-        origin: { y: 0.6 },
-        decay: 0.9,
-        scalar: 1.6,
+        // Inicia os confetes com mais partículas e duração maior
+        confetti({
+            particleCount: 600,
+            spread: 250,
+            origin: { y: 0.6 },
+            decay: 0.9, // diminui a velocidade de desaparecimento dos confetes
+            scalar: 1.6, // aumenta o tamanho dos confetes
+        });
+
+        // Adiciona um segundo disparo de confetes para um efeito prolongado
+        setTimeout(function() {
+            confetti({
+                particleCount: 600,
+                spread: 250,
+                origin: { y: 0.6 },
+                decay: 0.9,
+                scalar: 1.6,
+            });
+        }, 1000); // Dispara novamente após 1 segundo
     });
-}, 1000); // Dispara novamente após 1 segundo
 });
